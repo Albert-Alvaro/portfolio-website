60a960cba5df68f4eb445e0cac52449fecb322a4
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-05-03 00:18:42 +0800

    fixed issue with package dependencies with npm

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
-import Projects from './components/Projects';
-
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('../db/mydb.db', sqlite3.OPEN_READWRITE, (err)=> {
-  if (err) return console.error(err.message);
-});
+import Projects from './components/Projects'
 
 function App() {
 

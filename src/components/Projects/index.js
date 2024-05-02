de8be948cbd8014647a2aee2080c84c20b5c474f
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-05-02 20:02:44 +0800

    mostly finished projects page, need to create component for read more page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,9 +8,7 @@ import p3_1 from "../../assets/images/p3-1.png"
 import p4_1 from "../../assets/images/p4-1.png"
 import p5_1 from "../../assets/images/p5-1.png"
 import p6_1 from "../../assets/images/p6-1.png"
-
-
-
+import {motion} from "framer-motion"
 
 const Projects = () => {
     const [letterClass, setLetterClass] = useState('text-animated')
@@ -36,34 +34,106 @@ const Projects = () => {
                 <div className="square-icon">
                     <img src={p1_1} alt="recipe"/>
                     <h4>Recipe Generation App</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p2_1} alt="survey"/>
                     <h4>Survey Question Creator </h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p3_1} alt="chat"/>
                     <h4>AI Chatbot</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p4_1} alt="id8"/>
                     <h4>University Club Website</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p5_1} alt="android"/>
                     <h4>Android Apps</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p6_1} alt="pyja"/>
                     <h4>Python and Java Projects</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p6_1} alt="pyja"/>
                     <h4>Python and Java Projects</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
                 <div className="square-icon">
                     <img src={p6_1} alt="pyja"/>
                     <h4>Python and Java Projects</h4>
+                    <div className="button-modal">
+                        <motion.button
+                            whileHover={{ scale: 1.1}}
+                            whileTap={{scale:0.9}}
+                            className="button-1"
+                        >
+                            READ MORE
+                        </motion.button>
+                    </div>
                 </div>
             </div>
         </div>
